f18623c61612731c327e43104ed46677e78a2821
Reinoud Kruithof <reinoud.kruithof@gmail.com>
2015-11-26 17:16:30 +0100

    Crude implementation
    - Implemented polling script and defined wercker-step
    - Added wercker file

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+SERVICE=$WERCKER_SERVICE_CHECK_SERVICE
+PORT=$WERCKER_SERVICE_CHECK_PORT
+TIMEOUT=$WERCKER_SERVICE_CHECK_TIMEOUT
+INTERVAL=$WERCKER_SERVICE_CHECK_INTERVAL
+
+STEPS=$((TIMEOUT / INTERVAL))
+INTERVAL_S=$("${INTERVAL}/1000" | bc -l);
+
+echo -e "\e[1mChecking if service $SERVICE is reachable on port $PORT \e[21m"
+
+i=0; SERVICE_ONLINE=0;
+while ((i < STEPS)) && ((200!=SERVICE_ONLINE))
+do
+  printf "."
+  SERVICE_ONLINE=$(curl -s -o /dev/null -w "%{http_code}" -H "Accept: application/json" "DOCKER_${SERVICE}_PORT_${PORT}_TCP_ADDR:${PORT}")
+  sleep "${INTERVAL_S}"
+done
+
+if ((200!=SERVICE_ONLINE))
+then
+  echo -e "\e[31mCould not connect to service $SERVICE \e[39m" >&2
+  exit 1
+else
+  echo -e "\n\e[32m Service $SERVICE online! \e[39m"
+fi
